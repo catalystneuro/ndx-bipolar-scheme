900cfc4a5ac5ec45b50d1464ecff8b22f7b4a9a7
Ben Dichter <ben.dichter@gmail.com>
2019-11-18 16:25:48 -0800

    rename ext and index into anodes

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -9,26 +9,30 @@ def main():
     # these arguments were auto-generated from your cookiecutter inputs
     ns_builder = NWBNamespaceBuilder(
         doc='An NWB:N extension for storing bipolar referencing schema',
-        name='ndx-bipolar-referencing',
+        name='ndx-bipolar-scheme',
         version='0.1.0',
         author=list(map(str.strip, 'Ben Dichter'.split(','))),
         contact=list(map(str.strip, 'ben.dichter@gmail.com'.split(',')))
     )
 
-    for type_name in ('LabMetaData', 'DynamicTableRegion', 'DynamicTable'):
+    for type_name in ('LabMetaData', 'DynamicTableRegion', 'DynamicTable', 'VectorIndex'):
         ns_builder.include_type(type_name, namespace='core')
 
-    ecephys_ext = NWBGroupSpec(name='extracellular_electrophysiology_extensions',
+    ecephys_ext = NWBGroupSpec(name='extracellular_ephys_extensions',
                                neurodata_type_def='EcephysExt',
                                neurodata_type_inc='LabMetaData',
                                doc='Group that holds proposed extracellular electrophysiology extensions.')
-    bipolar_reference_scheme = ecephys_ext.add_group(name='bipolar_reference_scheme',
+    bipolar_reference_scheme = ecephys_ext.add_group(name='bipolar_scheme',
                                                      neurodata_type_inc='DynamicTable',
                                                      doc='Table that holds information about the bilpolar referencing '
                                                          'scheme used')
-    bipolar_reference_scheme.add_dataset(name='anode',
+    bipolar_reference_scheme.add_dataset(name='anodes',
                                          neurodata_type_inc='DynamicTableRegion',
                                          doc='references the electrodes table')
+    bipolar_reference_scheme.add_dataset(name='anodes_index',
+                                         neurodata_type_inc='VectorIndex',
+                                         doc='Index into anodes',
+                                         quantity='?')
     bipolar_reference_scheme.add_dataset(name='cathode',
                                          neurodata_type_inc='DynamicTableRegion',
                                          doc='references the electrodes table')
