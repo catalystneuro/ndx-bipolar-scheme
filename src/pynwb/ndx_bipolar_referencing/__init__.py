021ebf39685f45fdc275f40201bb5e094932ce4e
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-04-22 09:51:40 -0400

    Update __init__.py

--- src/pynwb/ndx_bipolar_referencing/__init__.py
+++ src/pynwb/ndx_bipolar_referencing/__init__.py
@@ -5,7 +5,7 @@ from pynwb import load_namespaces, get_class
 ndx_bipolar_scheme_specpath = os.path.join(
     os.path.dirname(__file__),
     'spec',
-    'ndx-bipolar.namespace.yaml'
+    'ndx-bipolar-scheme.namespace.yaml'
 )
 
 # If the extension has not been installed yet but we are running directly from
@@ -15,10 +15,10 @@ if not os.path.exists(ndx_bipolar_scheme_specpath):
         os.path.dirname(__file__),
         '..', '..', '..',
         'spec',
-        'ndx-bipolar.namespace.yaml'
+        'ndx-bipolar-scheme.namespace.yaml'
     ))
 
 # Load the namespace
 load_namespaces(ndx_bipolar_scheme_specpath)
 
-EcephysExt = get_class('EcephysExt', 'ndx-bipolar')
+EcephysExt = get_class('EcephysExt', 'ndx-bipolar-scheme')
