bf17b0194ffd2b98cad352308393d162016f0468
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-04-28 14:23:49 -0400

    Update create_extension_spec.py

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -29,7 +29,7 @@ def main():
 
     bipolar_scheme = ecephys_ext.add_group(
         name='bipolar_scheme',
-        data_type_def='BipolarScheme',
+        data_type_def='BipolarSchemeTable',
         data_type_inc='DynamicTable',
         doc='Table that holds information about the bipolar scheme used'
     )
@@ -69,74 +69,6 @@ def main():
     )
 
     new_data_types = [ecephys_ext]
-
-    # export the spec to yaml files in the spec folder
-    output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
-    export_spec(ns_builder, new_data_types, output_dir)
-
-
-    BipolarSchemeTable = GroupSpec(
-        doc='type for storing time-varying 3D point clouds',
-        data_type_def='PointCloudTable',
-        data_type_inc='DynamicTable',
-        default_name='BipolarSchemeTable'
-    )
-
-    BipolarSchemeTable.add_dataset(
-        name='timestamps',
-        data_type_inc='VectorData',
-        doc='time of each frame in seconds',
-        dims=('num_frames',),
-        shape=(None,),
-        dtype='float')
-
-    BipolarSchemeTable.add_dataset(
-        name='point_cloud',
-        data_type_inc='VectorData',
-        doc='datapoints locations over time',
-        dims=('time', '[x, y, z]'),
-        shape=(None, 3),
-        dtype='float',
-    )
-
-    BipolarSchemeTable.add_dataset(
-        name='point_cloud_index',
-        data_type_inc='VectorIndex',
-        doc='datapoints indices',
-        dims=('index',),
-        shape=(None,),
-    )
-
-    BipolarSchemeTable.add_dataset(
-        name='color',
-        data_type_inc='VectorData',
-        doc='datapoints color',
-        dims=('time', '[r, g, b]'),
-        shape=(None, 3),
-        dtype='float',
-        quantity='?'
-    )
-
-    BipolarSchemeTable.add_dataset(
-        name='color_index',
-        data_type_inc='VectorIndex',
-        doc='datapoints colors indices',
-        dims=('index',),
-        shape=(None,),
-        quantity='?'
-    )
-
-    BipolarSchemeTable.add_attribute(
-        name='colnames',
-        dims=('num_columns',),
-        shape=(None,),
-        doc='The names of the columns in this table. This should be used to specify '
-            'an order to the columns.',
-        default_value=('point_cloud',),
-        dtype='text'
-    )
-
-    new_data_types = [BipolarSchemeTable]
 
     # export the spec to yaml files in the spec folder
     output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
